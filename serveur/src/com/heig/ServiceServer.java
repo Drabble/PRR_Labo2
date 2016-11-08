8f069ac87fc62d46dca3d5300e3791890ab71fd2
servietsky777 <antoine.drabble@gmail.com>
2016-11-08 17:32:07 +0100

    Add protocol to the service server

--- serveur/src/com/heig/ServiceServer.java
+++ serveur/src/com/heig/ServiceServer.java
@@ -22,7 +22,7 @@ public class ServiceServer {
             new Linker("127.0.0.1", 7782)
     };
 
-    byte[] serviceNumber = {0};
+    byte idService = 0;
 
     final int pointToPointPort = 1234; // TODO : Make this an argument
 
@@ -44,7 +44,7 @@ public class ServiceServer {
      * @throws InterruptedException
      */
     public void start() throws IOException, InterruptedException {
-
+        // Variable used to generate random values
         Random rand = new Random();
 
         // Create point to point socket to send messages to the linker
@@ -53,7 +53,7 @@ public class ServiceServer {
 
         // Subscribe to the linker
         int linkerNumber = rand.nextInt(linkers.length);
-        DatagramPacket linkerSubscribePacket = new DatagramPacket(serviceNumber, serviceNumber.length, InetAddress.getByName(linkers[linkerNumber].getIp()),  linkers[linkerNumber].getPort());
+        DatagramPacket linkerSubscribePacket = new DatagramPacket(new byte[]{idService, (byte)Protocol.SUB.ordinal()}, 2, InetAddress.getByName(linkers[linkerNumber].getIp()),  linkers[linkerNumber].getPort());
         pointToPointSocket.send(linkerSubscribePacket);
 
         // Wait for confirmation
@@ -61,16 +61,18 @@ public class ServiceServer {
         DatagramPacket linkerConfirmationPacket = new DatagramPacket(buffer, buffer.length);
         pointToPointSocket.receive(linkerConfirmationPacket);
 
-        // Do service forever
-        while(true){
-            // Wait for client request
-            byte[] buffer2 = new byte[4];
-            DatagramPacket clientPacket = new DatagramPacket(buffer2, buffer2.length);
-            pointToPointSocket.receive(clientPacket);
+        if(linkerConfirmationPacket.getData()[1] == Protocol.CONFIRM_SUB.ordinal()) {
+            // Do service forever
+            while (true) {
+                // Wait for client request
+                byte[] buffer2 = new byte[4];
+                DatagramPacket clientPacket = new DatagramPacket(buffer2, buffer2.length);
+                pointToPointSocket.receive(clientPacket);
 
-            // Answer to client request
-            DatagramPacket clientResponsePacket = new DatagramPacket(clientPacket.getData(), clientPacket.getData().length, clientPacket.getAddress(), clientPacket.getPort());
-            pointToPointSocket.send(clientResponsePacket);
+                // Answer to client request
+                DatagramPacket clientResponsePacket = new DatagramPacket(clientPacket.getData(), clientPacket.getData().length, clientPacket.getAddress(), clientPacket.getPort());
+                pointToPointSocket.send(clientResponsePacket);
+            }
         }
     }
 
